d54076a305cea302db878779441e3da441fb77d0
D. Prameswara <meshwara@gmail.com>
2020-12-21 12:54:34 +0700

    Custom bootstrap bundle

---
+++ bootstrap.zee.js
@@ -0,0 +1,4 @@
+// @prepros-append ./node_modules/bootstrap/js/dist/button.js;
+// @prepros-append ./node_modules/bootstrap/js/dist/dropdown.js;
+// @prepros-append ./node_modules/bootstrap/js/dist/scrollspy.js;
+// @prepros-append ./node_modules/bootstrap/js/dist/util.js;
