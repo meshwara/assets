a91aef6b465eabd769a76175189c60d307d1d94e
D. Prameswara <meshwara@gmail.com>
2020-12-21 13:53:29 +0700

    Slim kan bootstrap js, missing collapse

--- bootstrap.zee.js
+++ bootstrap.zee.js
@@ -1,4 +1,3 @@
 // @prepros-append ./node_modules/bootstrap/js/dist/button.js;
-// @prepros-append ./node_modules/bootstrap/js/dist/dropdown.js;
-// @prepros-append ./node_modules/bootstrap/js/dist/scrollspy.js;
 // @prepros-append ./node_modules/bootstrap/js/dist/util.js;
+// @prepros-append ./node_modules/bootstrap/js/dist/collapse.js;
